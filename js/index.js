7de526edc836addcfeda12a8cc6efb4b73e48cad
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2024-06-08 21:11:05 -0600

    Added Google Analytics tag
    Updated links in footer

--- js/index.js
+++ js/index.js
@@ -9,3 +9,8 @@ $(document).ready(function()
    $("#Massage-and-services-slideshow").conveyerbelt({speed:2, spacing: 8});
    $("#Call-now-button").button({icon: 'ui-primary', iconPosition: 'beginning'});
 });
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-S3DTHKED1J');
