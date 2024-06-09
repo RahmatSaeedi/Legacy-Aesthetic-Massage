ce18cde76fe9eb3e745ac3460901120e659502b4
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2024-06-09 12:52:55 -0600

    Page additions - multipage

--- js/Cupping---Acne-Therapy--Dark-Patch-Removal.js
+++ js/Cupping---Acne-Therapy--Dark-Patch-Removal.js
@@ -1,5 +1,7 @@
 $(document).ready(function()
 {
+   $("#Massage-and-services-slideshow").conveyerbelt({speed:2, spacing: 8});
+   $("#Call-now-button").button({icon: 'ui-primary', iconPosition: 'beginning'});
    $('#Overlay-menu-overlay').overlay({hideTransition:true});
    $('.Overlay-menu').drilldownmenu({backText: 'Back'});
    $('.Overlay-menu .drilldown-back > a').addClass('wb-theme-button');
@@ -8,8 +10,6 @@ $(document).ready(function()
       $.overlay.toggle($('#Overlay-menu-overlay'));
       return false;
    });
-   $("#Massage-and-services-slideshow").conveyerbelt({speed:2, spacing: 8});
-   $("#Call-now-button").button({icon: 'ui-primary', iconPosition: 'beginning'});
 });
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
